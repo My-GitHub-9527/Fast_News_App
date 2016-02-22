edc6de8bb9789d742816906934b28d00915ba178
My-GitHub-9527 <276626645@qq.com>
2016-02-22 08:43:07 +0800

    test1

--- Fast_News/src/main/java/com/news/fast_news_app/MainActivity.java
+++ Fast_News/src/main/java/com/news/fast_news_app/MainActivity.java
@@ -2,7 +2,6 @@ package com.news.fast_news_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,16 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "这是第二次修改");
-        Log.d("MainActivity", "这是第二次修改");
-        Log.d("MainActivity", "这是第二次修改");
-        Log.d("MainActivity", "这是第二次修改");
-        Log.d("MainActivity", "这是第二次修改");
-        Log.d("MainActivity", "这是第二次修改");
-        Log.d("MainActivity", "这是第二次修改");
-        Log.d("MainActivity", "这是第二次修改");
-        Log.d("MainActivity", "这是第二次修改");
-        Log.d("MainActivity", "这是第二次修改");
-        Log.d("MainActivity", "这是第二次修改");
+
     }
 }
