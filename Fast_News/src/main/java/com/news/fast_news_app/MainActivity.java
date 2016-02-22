e4809d28dcd261f240d394fe6eb450fac4cf709f
My-GitHub-9527 <276626645@qq.com>
2016-02-22 08:37:17 +0800

    test1

--- Fast_News/src/main/java/com/news/fast_news_app/MainActivity.java
+++ Fast_News/src/main/java/com/news/fast_news_app/MainActivity.java
@@ -2,8 +2,7 @@ package com.news.fast_news_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import org.jsoup.Jsoup;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Jsoup.connect("www.baidu.com");
+		ImageButton iv = new ImageButton(this);
     }
 }
