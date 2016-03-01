500d6183e50dc4f43a77c812748672e91f49055f
My-GitHub-9527 <276626645@qq.com>
2016-03-01 12:04:52 +0800

    所以界面基本完成

--- Fast_News/src/main/java/com/news/fast_news_app/bottom_fragment/Bottom_Fourth.java
+++ Fast_News/src/main/java/com/news/fast_news_app/bottom_fragment/Bottom_Fourth.java
@@ -6,17 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.news.fast_news_app.R;
+import com.news.fast_news_app.adapter.Tab4_Adapter;
 
 /**
  * Created by Administrator on 2016/2/22.
  */
 public class Bottom_Fourth extends Fragment {
+
+    private ListView tab4_listview;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View bottom_view4 = inflater.inflate(R.layout.tabhost_view4,null);
+        tab4_listview = (ListView) bottom_view4.findViewById(R.id.tab4_listview);
+        tab4_listview.setAdapter(new Tab4_Adapter(getActivity()));
         return bottom_view4;
     }
 }
